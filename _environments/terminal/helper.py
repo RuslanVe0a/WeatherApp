c900fafd2eece15e308738c5edf27acc5e9e13a1
RuslanVe0a <venelinovruslan11@gmail.com>
2025-08-19 12:29:29 +0300

    Initial commit

--- _environments/terminal/helper.py
+++ _environments/terminal/helper.py
@@ -3,4 +3,8 @@ import argparse
 
 
 def add_subparsers(subparsers: argparse._SubParsersAction) -> None:
-    pass
+    _config_parser = subparsers.add_parser("config", help="Configure environment")
+    _config_parser.add_argument("-a", "--api", help="Provide an API key", default=None, dest="api", required=False)
+    _config_parser.add_argument("-s", "--service", help="Provide a service key", default=None, dest="service", required=False)
+    _config_parser.add_argument("-c", "--configPath", help="Provide a configuration path", default=None, dest="configPath", required=False)
+    _config_parser.add_argument("-sp", "--servicePath", help="Provide a service path (i.e /v1/current.xml)", default=None, dest="servicePath", required=False)
--- _environments/terminal/terminal.py
+++ _environments/terminal/terminal.py
@@ -13,10 +13,16 @@ class Terminal(object):
         return "Terminal"
 
     def load_base(self) -> None:
+        """
+        The purpose of this method is to create all the essential components of the terminal.
+        The user is required to provide a query. It may be also required to configure the configuration module,
+        however if not - config.ini will be used instead.
+        :return:
+        """
         self.parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode", default=False, dest="verbose")
         self.parser.add_argument("-t", "--test", action="store_true", help="Run tests", default=False, dest="test")
         self.parser.add_argument("-q", "--query", action="store", help="Provide a target query (i.e Shumen)", default=None, dest="target", required=False)
-        _startparser = self.parser.add_subparsers(title = "Config", dest = "config")
+        _startparser = self.parser.add_subparsers(title = "Features", dest = "features", help = "Features commands")
         helper.add_subparsers(_startparser)
 
     def finish(self):
